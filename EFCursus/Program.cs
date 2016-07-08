067a90837bd4df4d42e09bf4d08ce8994f7e4e7e
benvanhoutvinck <vanhoutvinckben@gmail.com>
2016-07-08 10:18:08 +0200

    first commit

---
+++ EFCursus/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace EFCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Familienaam:");
+            var familienaam = Console.ReadLine();
+            using (var entities = new OpleidingenEntities())
+            {
+                var aantalDocenten = entities.AantalDocentenMetFamilienaam(familienaam);
+                Console.WriteLine("{0} docent(en)", aantalDocenten.First());
+            }
+        }
+        void VoorraadBijvulling(int artikelNr, int magazijnNr, int aantalStuks)
+        {
+            using (var entities = new OpleidingenEntities())
+            {
+                var voorraad = entities.Voorraden.Find(magazijnNr, artikelNr);
+                if (voorraad != null)
+                {
+                    voorraad.AantalStuks += aantalStuks;
+                    Console.WriteLine("Pas nu de voorraad aan met de Server Explorer," +
+                    " druk daarna op Enter");
+                    Console.ReadLine();
+                    try
+                    { 
+                    entities.SaveChanges();
+                        }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        Console.WriteLine("Voorraad werd door andere applicatie aangepast.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Voorraad niet gevonden");
+                }
+            }
+        }
+
+        void VoorraadTransfer(int artikelNr, int vanMagazijnNr, int naarMagazijnNr, int aantalStuks)
+        {
+            var transactionOptions = new TransactionOptions
+            {
+                IsolationLevel = System.Transactions.IsolationLevel.RepeatableRead
+            };
+            using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
+            {
+                using (var entities = new OpleidingenEntities())
+                {
+                    var vanVoorraad = entities.Voorraden.Find(vanMagazijnNr, artikelNr);
+                    if (vanVoorraad != null)
+                    {
+                        if (vanVoorraad.AantalStuks >= aantalStuks)
+                        {
+                            vanVoorraad.AantalStuks -= aantalStuks;
+                            var naarVoorraad = entities.Voorraden.Find(naarMagazijnNr, artikelNr);
+                            if (naarVoorraad != null) // voorraad aanpassen
+                            {
+                                naarVoorraad.AantalStuks += aantalStuks;
+                            }
+                            else // nieuwe voorraad aanmaken
+                            {
+                                naarVoorraad = new Voorraad
+                                {
+                                    ArtikelNr = artikelNr,
+                                    MagazijnNr = naarMagazijnNr,
+                                    AantalStuks = aantalStuks
+                                };
+                                entities.Voorraden.Add(naarVoorraad);
+                            }
+                            entities.SaveChanges();
+                            transactionScope.Complete();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Te weinig voorraad voor transfer");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Artikel niet gevonden in magazijn {0}", vanMagazijnNr);
+                    }
+                }
+            }
+        }
+    }
+}
